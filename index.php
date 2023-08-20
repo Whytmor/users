7f23d25cb7947f2fe71a784af272cdb5a46b27b0
Whytmor <makedonskija297@gmail.com>
2023-08-20 22:20:58 +0300

    begin

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+error_reporting(-1);
+session_start();
+
+require __DIR__ . '/src/models/registry_handler.php';
+require __DIR__ . '/views/registry.php';
---
+++ views/registry.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+  </head>
+  <body>
+    <form action="../src/models/registry_handler.php" method="post">
+    user_name<input type="text" name="user_name"><br>
+    password<inpu type="text" name="password"><br>
+    phone<input type="text" name="phone"><br>
+    email<input type="text" name="email"><br>
+    <button type="submit">Зарегистрироваться</button>
+</form>
+   </body>
+</html>
